05747c2176dea2682c30dc7b623fecd672af6218
ip99202 <ip99202@naver.com>
2020-07-23 18:19:16 +0900

    Delete search

--- cukprj/src/App.vue
+++ cukprj/src/App.vue
@@ -9,14 +9,6 @@
       <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
         <span class="hidden-sm-and-down">Google Contacts</span>
       </v-toolbar-title>
-      <v-text-field
-        flat
-        solo-inverted
-        hide-details
-        prepend-inner-icon="mdi-magnify"
-        label="Search"
-        class="hidden-sm-and-down"
-      ></v-text-field>
       <v-spacer></v-spacer>
       <v-btn icon>
         <v-icon>mdi-apps</v-icon>
