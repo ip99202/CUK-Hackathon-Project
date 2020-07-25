b25280da2695072e7eaa27c33147e22435d47597
ip99202 <ip99202@naver.com>
2020-07-25 22:50:38 +0900

    Create go top button

--- cukprj/src/App.vue
+++ cukprj/src/App.vue
@@ -9,14 +9,10 @@
       <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
         <span class="hidden-sm-and-down">어울림</span>
       </v-toolbar-title>
-    
+
       <v-spacer></v-spacer>
-      <v-tabs
-      dark
-      right
-      v-model="tab"
-      >
-      <v-tab v-for="item in items" :key="item">{{item}}</v-tab>
+      <v-tabs dark right v-model="tab">
+        <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
       </v-tabs>
     </v-app-bar>
 
@@ -28,34 +24,37 @@
           <v-tab-item><product-page></product-page></v-tab-item>
           <v-tab-item><process-page></process-page></v-tab-item>
           <v-tab-item><market-analysis-page></market-analysis-page></v-tab-item>
-      </v-tabs-items>
+        </v-tabs-items>
       </v-container>
     </v-main>
-    <v-btn bottom color="pink" dark fab fixed right>
-      <v-icon>mdi-plus</v-icon>
-    </v-btn>
-    
+    <back-to-top bottom="50px" right="50px">
+      <v-btn color="primary" fab small dark>
+      <v-icon>mdi-arrow-up-bold</v-icon>
+      </v-btn>
+    </back-to-top>
   </v-app>
 </template>
 
 <script>
-import ProductPage from './components/Page/ProductPage.vue'
-import ProcessPage from './components/Page/ProcessPage.vue'
-import MarketAnalysisPage from './components/Page/MarketAnalysisPage.vue'
+import ProductPage from "./components/Page/ProductPage.vue";
+import ProcessPage from "./components/Page/ProcessPage.vue";
+import MarketAnalysisPage from "./components/Page/MarketAnalysisPage.vue";
+import BackToTop from "vue-backtotop";
 
 export default {
-  name: 'mainPage',
+  name: "mainPage",
   components: {
     ProductPage,
     ProcessPage,
     MarketAnalysisPage,
+    BackToTop,
   },
   props: {
     source: String,
   },
   data: () => ({
     tab: null,
-    items: ['Home', '제품소개', '제품원리', '시장분석'],
+    items: ["Home", "제품소개", "제품원리", "시장분석"],
   }),
 };
 </script>
--- cukprj/src/components/Page/ProcessPage.vue
+++ cukprj/src/components/Page/ProcessPage.vue
@@ -1,95 +1,147 @@
 <template>
   <v-container>
+    <v-card class="mx-auto" flat>
+      <v-container>
+        <v-row justify="">
+          <v-col cols="auto">
+            <v-img
+              height="350"
+              width="350"
+              src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+            ></v-img>
+          </v-col>
+          <v-col>
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
 
-  <v-card class="mx-auto" flat>
-    <v-container>
-      <v-row justify="">
-        <v-col cols="auto">
-          <v-img
-            height="350"
-            width="350"
-            src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
-          ></v-img>
-        </v-col>
-    <v-col>
-      <v-card-title>Top 10 Australian beaches</v-card-title>
-    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>Whitehaven Beach</div>
 
-    <v-card-text class="text--primary">
-      <div>Whitehaven Beach</div>
+              <div>Whitsunday Island, Whitsunday Islands</div>
+            </v-card-text>
 
-      <div>Whitsunday Island, Whitsunday Islands</div>
-    </v-card-text>
+            <v-card-actions>
+              <v-btn color="orange" text>
+                Share
+              </v-btn>
 
-    <v-card-actions>
-      <v-btn
-        color="orange"
-        text
-      >
-        Share
-      </v-btn>
+              <v-btn color="orange" text>
+                Explore
+              </v-btn>
+            </v-card-actions>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
 
-      <v-btn
-        color="orange"
-        text
-      >
-        Explore
-      </v-btn>
-    </v-card-actions>
-    </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
+    <v-card class="mx-auto" flat>
+      <v-container>
+        <v-row justify="right">
+          <v-col>
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
 
-  <v-card class="mx-auto" flat>
-    <v-container>
-      <v-row justify="right">
-    <v-col>
-      <v-card-title>Top 10 Australian beaches</v-card-title>
-    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+            <v-card-text class="text--primary">
+              <div>Whitehaven Beach</div>
 
-    <v-card-text class="text--primary">
-      <div>Whitehaven Beach</div>
+              <div>Whitsunday Island, Whitsunday Islands</div>
+            </v-card-text>
 
-      <div>Whitsunday Island, Whitsunday Islands</div>
-    </v-card-text>
+            <v-card-actions>
+              <v-btn color="orange" text>
+                Share
+              </v-btn>
 
-    <v-card-actions>
-      <v-btn
-        color="orange"
-        text
-      >
-        Share
-      </v-btn>
+              <v-btn color="orange" text>
+                Explore
+              </v-btn>
+            </v-card-actions>
+          </v-col>
+          <v-col cols="auto">
+            <v-img
+              height="350"
+              width="350"
+              src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+            ></v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
 
-      <v-btn
-        color="orange"
-        text
-      >
-        Explore
-      </v-btn>
-    </v-card-actions>
-    </v-col>
-    <v-col cols="auto">
-          <v-img
-            height="350"
-            width="350"
-            src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
-          ></v-img>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
- 
+    <v-card class="mx-auto" flat>
+      <v-container>
+        <v-row justify="right">
+          <v-col>
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+            <v-card-text class="text--primary">
+              <div>Whitehaven Beach</div>
+
+              <div>Whitsunday Island, Whitsunday Islands</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="orange" text>
+                Share
+              </v-btn>
+
+              <v-btn color="orange" text>
+                Explore
+              </v-btn>
+            </v-card-actions>
+          </v-col>
+          <v-col cols="auto">
+            <v-img
+              height="350"
+              width="350"
+              src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+            ></v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+
+    <v-card class="mx-auto" flat>
+      <v-container>
+        <v-row justify="right">
+          <v-col>
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+            <v-card-text class="text--primary">
+              <div>Whitehaven Beach</div>
+
+              <div>Whitsunday Island, Whitsunday Islands</div>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-btn color="orange" text>
+                Share
+              </v-btn>
+
+              <v-btn color="orange" text>
+                Explore
+              </v-btn>
+            </v-card-actions>
+          </v-col>
+          <v-col cols="auto">
+            <v-img
+              height="350"
+              width="350"
+              src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+            ></v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'ProductPage',
+export default {
+  name: "ProductPage",
 
-    data: () => ({
-      
-    }),
-  }
+  data: () => ({}),
+};
 </script>
