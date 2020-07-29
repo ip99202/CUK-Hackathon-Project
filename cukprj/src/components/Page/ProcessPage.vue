eaffd2e78b7f2ce7891296ab1f9e92f5b2d5c860
ip99202 <ip99202@naver.com>
2020-07-29 14:18:18 +0900

    Fix process page

--- cukprj/src/components/Page/ProcessPage.vue
+++ cukprj/src/components/Page/ProcessPage.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-card max-width="900" class="mx-auto" flat>
       <v-container>
-        <v-row justify="">
+        <v-row justify="space-between">
           <v-col cols="auto">
             <v-img
               src="https://user-images.githubusercontent.com/52627952/88545546-18cbdd80-d056-11ea-9b45-5a2d2a9bebcb.gif"
@@ -12,34 +12,58 @@
       </v-container>
     </v-card>
 
-    <v-card max-width="900" class="mx-auto" flat>
+    <v-card max-width="700" class="mx-auto" flat>
       <v-container>
-        <v-row justify="right">
-          <v-col>
-            <v-card-title>Top 10 Australian beaches</v-card-title>
-            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-            <v-card-text class="text--primary">
-              <div>Whitehaven Beach</div>
-
-              <div>Whitsunday Island, Whitsunday Islands</div>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-btn color="orange" text>
-                Share
-              </v-btn>
-
-              <v-btn color="orange" text>
-                Explore
-              </v-btn>
-            </v-card-actions>
-          </v-col>
+        <v-row justify="space-between">
           <v-col cols="auto">
             <v-img
-              height="350"
-              width="350"
-              src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+              width="200px"
+              src="https://user-images.githubusercontent.com/52627952/88758179-c9df8e80-d1a2-11ea-9ba3-e90a01c6de1d.png"
+            ></v-img>
+          </v-col>
+        </v-row>
+        <v-row justify="space-between">
+          <v-col cols="auto">
+            <v-img
+              width="200px"
+              src="https://user-images.githubusercontent.com/52627952/88758169-c815cb00-d1a2-11ea-8516-b65198aa9fa9.jpg"
+            ></v-img>
+          </v-col>
+        </v-row>
+        <v-row justify="space-between">
+          <v-col cols="auto">
+            <v-img
+              width="200px"
+              src="https://user-images.githubusercontent.com/52627952/88758175-c946f800-d1a2-11ea-89af-12617a0900b8.png"
+            ></v-img>
+          </v-col>
+        </v-row>
+        <v-row justify="space-between">
+          <v-col cols="auto">
+            <v-img
+              width="200px"
+              src="https://user-images.githubusercontent.com/52627952/88758176-c9df8e80-d1a2-11ea-879c-7a918abc6309.png"
+            ></v-img>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+
+    <v-card max-width="900" class="mx-auto" flat>
+      <v-container>
+        <v-row justify="space-between">
+          <v-col cols="auto">
+            송신기
+            <v-img
+            width="400px"
+              src="https://user-images.githubusercontent.com/52627952/88759264-5d19c380-d1a5-11ea-81e3-8828badb2c3d.jpg"
+            ></v-img>
+          </v-col>
+          <v-col cols="auto">
+            수신기
+            <v-img
+            width="400px"
+              src="https://user-images.githubusercontent.com/52627952/88759267-5e4af080-d1a5-11ea-9e58-009b8e45e1ba.png"
             ></v-img>
           </v-col>
         </v-row>
@@ -79,40 +103,7 @@
         </v-row>
       </v-container>
     </v-card>
-
-    <v-card max-width="900" class="mx-auto" flat>
-      <v-container>
-        <v-row justify="right">
-          <v-col>
-            <v-card-title>Top 10 Australian beaches</v-card-title>
-            <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
-
-            <v-card-text class="text--primary">
-              <div>Whitehaven Beach</div>
-
-              <div>Whitsunday Island, Whitsunday Islands</div>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-btn color="orange" text>
-                Share
-              </v-btn>
-
-              <v-btn color="orange" text>
-                Explore
-              </v-btn>
-            </v-card-actions>
-          </v-col>
-          <v-col cols="auto">
-            <v-img
-              height="350"
-              width="350"
-              src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
-            ></v-img>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+    <br><br>
     <h6 class="text-center">
       아이콘 제작자
       <a
